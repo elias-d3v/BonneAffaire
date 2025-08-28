1a1a40d3885ff4ca75e28ab56ab889668e1044c0
Elias-DEV <156446201+elias-d3v@users.noreply.github.com>
2025-08-28 18:17:26 +0200

    Sprint 3 et 4

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
 
 #[Route('/post')]
 class PostController extends AbstractController
@@ -52,15 +53,27 @@ class PostController extends AbstractController
     }
 
     #[Route('/list', name: 'post_list')]
-    public function list(PostRepository $repo): Response
+    public function list(PostRepository $repo, Request $request, CategoryRepository $catRepo): Response
     {
-        $posts = $repo->findAll();
+        $sort = $request->query->get('sort', 'date');
+        $categoryId = $request->query->get('category');
+        $categoryId = $categoryId ? (int) $categoryId : null;
+
+        $dept = $request->query->get('dept');
+
+        $posts = $repo->findAllSorted($sort, $categoryId, $dept);
+        $categories = $catRepo->findAll();
+
         return $this->render('post/list.html.twig', [
             'posts' => $posts,
+            'sort' => $sort,
+            'categories' => $categories,
+            'selectedCategory' => $categoryId,
+            'selectedDept' => $dept,
         ]);
     }
 
-    #[Route('/{id}', name: 'post_show')]
+    #[Route('/{id<\d+>}', name: 'post_show')]
     public function show(Post $post): Response
     {
         return $this->render('post/show.html.twig', [
@@ -77,13 +90,15 @@ class PostController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
             // On récupère les fichiers
             $imageFiles = [
                 'image1' => $form['image1']->getData(),
                 'image2' => $form['image2']->getData(),
                 'image3' => $form['image3']->getData(),
             ];
-             foreach ($imageFiles as $field => $imageFile) {
+
+            foreach ($imageFiles as $field => $imageFile) {
                 if ($imageFile) {
                     $newFilename = uniqid().'.'.$imageFile->guessExtension();
                     $imageFile->move(
@@ -91,18 +106,38 @@ class PostController extends AbstractController
                         $newFilename
                     );
 
-                    // setter dynamique : setImage1, setImage2...
                     $setter = 'set'.ucfirst($field);
                     $post->$setter($newFilename);
                 }
             }
-            $em->flush(); // sauvegarde la modification
+            $em->flush(); 
             return $this->redirectToRoute('post_show', ['id' => $post->getId()]);
         }
 
         return $this->render('post/edit.html.twig', [
             'form' => $form,
             'post' => $post,
+        ]);
+    }
+
+    #[Route('/category/{id}', name: 'post_by_category')]
+    public function byCategory(PostRepository $repo, int $id): Response
+    {
+        $posts = $repo->findByCategory($id);
+        return $this->render('post/list.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/search', name: 'post_search')]
+    public function search(Request $request, PostRepository $repo): Response
+    {
+        $term = $request->query->get('q', '');
+        $posts = $term ? $repo->search($term) : [];
+
+        return $this->render('post/search.html.twig', [
+            'posts' => $posts,
+            'term' => $term,
         ]);
     }
 }
---
+++ src/Controller/ReportController.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Entity\Report;
+use App\Repository\ReportRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/report')]
+class ReportController extends AbstractController
+{
+    #[Route('/{id}', name: 'report_post')]
+    public function report(Post $post, EntityManagerInterface $em, ReportRepository $repo): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        // éviter de signaler deux fois la même annonce
+        $existing = $repo->findOneBy(['reporter' => $user, 'post' => $post]);
+
+        if (!$existing) {
+            $report = new Report();
+            $report->setAuthor($post->getUser());
+            $report->setReporter($user);
+            $report->setPost($post);
+            $report->setIsHandled(false);
+
+            $em->persist($report);
+            $em->flush();
+
+            $this->addFlash('success', 'Annonce signalée avec succès.');
+        } else {
+            $this->addFlash('warning', 'Vous avez déjà signalé cette annonce.');
+        }
+
+        return $this->redirectToRoute('post_show', ['id' => $post->getId()]);
+    }
+
+    #[Route('/list', name: 'report_list')]
+    public function list(ReportRepository $repo): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN'); // réservé admin
+
+        $reports = $repo->findAll();
+
+        return $this->render('report/list.html.twig', [
+            'reports' => $reports,
+        ]);
+    }
+}
